6877c9e692347f7fe5ad23fba259c77a0c75a8ad
Chriss Chris <zenskech@outlook.com>
2020-12-09 19:36:10 +0500

    Update Header.js

--- pic-images-site/src/components/Header.js
+++ pic-images-site/src/components/Header.js
@@ -3,7 +3,7 @@ import React from "react";
 function Header() {
   return (
     <header>
-      <h2>Pic Some</h2>
+      <h2>Choice Pic</h2>
       <i className="ri-shopping-cart-line ri-fw ri-2x"></i>
     </header>
   );
